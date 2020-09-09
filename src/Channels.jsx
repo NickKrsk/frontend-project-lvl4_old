d63202ae9baa5cdf735596d4f8aa72af25bf38eb
Nick <ggd43@rambler.ru>
2020-09-09 17:37:07 +0700

    y

--- src/Channels.jsx
+++ src/Channels.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import cn from 'classnames';
 
 export default class Channels extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      activeChannelName: "general"
+    };
+  }
+
+  selectChannel = (channelName) => () => {
+    //console.log(channelName);
+    this.setState({ activeChannelName: channelName});
+    //console.log(this.state.activeChannelName);
+  }
+
+  getButtonClassName = (channelName) => {
+    return cn('nav-link btn btn-block', {'active' : this.state.activeChannelName === channelName});
   }
 
   render() {
@@ -10,11 +24,18 @@ export default class Channels extends React.Component {
     return(   
      // <div className="shopping-list"></div>
      // <h1>Список покупок для </h1>
-        <ul>
+        <ul className='nav flex-column nav-pills nav-fill'>
           <>
-          {data.map(item => <li key={item.id}>{item.name}</li>)}
+          {data.map(item => 
+            <li className='nav-item' key={item.id}>
+              <button 
+                      onClick={this.selectChannel(item.name)} 
+                      className={this.getButtonClassName(item.name)}>
+                {item.name}
+              </button>
+            </li>)}
           </>
-        </ul>
+        </ul>        
     );
   }
 }
